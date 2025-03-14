97ff15f910a5bc94feb5e1a8599351a9ac825069
Caio De Souza Marinho <caide-so@c1r7p1.42sp.org.br>
2025-03-13 23:06:16 -0300

    2 philosophers test

--- philo/main.c
+++ philo/main.c
@@ -12,10 +12,57 @@
 
 #include "philo.h"
 
-int	main(int argc, char **argv)
+typedef struct	s_philo
 {
-	(void)argc;
-	(void)argv;
-	printf("salve\n");
+	int	index;
+	pthread_mutex_t	*mutex;
+	int	time_to_eat;
+	int	time_to_sleep;
+}	t_philo;
+
+void*	routine(void *arg)
+{
+	t_philo	*philo = (t_philo *)arg;
+
+	pthread_mutex_lock(philo->mutex);
+	printf("Philosofer %d is eating\n", philo->index);
+	printf("Philosofer %d is sleeping\n", philo->index);
+	printf("Philosofer %d is thinking\n", philo->index);
+	pthread_mutex_unlock(philo->mutex);
+	return (NULL);
+}
+
+void	init_philo(t_philo *philo1, t_philo *philo2, pthread_mutex_t *mutex)
+{
+	int	i;
+	
+	i = 1;
+	philo1->index = i;
+	philo1->mutex = mutex;
+	i++;
+	philo2->index = i;
+	philo2->mutex = mutex;
+}
+
+int	main(void)
+{
+	pthread_t tred1;
+	pthread_t tred2;
+
+	t_philo	philo1;
+	t_philo philo2;
+
+	pthread_mutex_t mutex;
+	pthread_mutex_init(&mutex, NULL);
+
+	init_philo(&philo1, &philo2, &mutex);
+
+	pthread_create(&tred1, NULL, routine, &philo1);
+	pthread_create(&tred2, NULL, routine, &philo2);
+
+	pthread_join(tred1, NULL);
+	pthread_join(tred2, NULL);
+	printf("============FIM===========\n");
+	pthread_mutex_destroy(&mutex);
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -13,6 +13,23 @@
 #ifndef PHILO_H 
 # define PHILO_H
 
+// to use memset
+#include <string.h>
+
+// to use printf
 #include <stdio.h>
+
+// to use malloc, free
+#include <stdlib.h>
+
+// to use function gettimeofday
+#include <sys/time.h>
+
+// to use write, usleep
+#include <unistd.h>
+
+// to use pthread_create, pthread_detach, pthread_join, pthread_mutex_init,
+// pthread_mutex_destroy, pthread_mutex_loc, pthread_mutex_unlock
+#include <pthread.h>
 
 #endif
